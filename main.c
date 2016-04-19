e325259702d702d8e3d1983a86fd985a8ca001f9
Daniel Engel <danielecc2594@gmail.com>
2016-04-19 14:57:22 -0400

    stage

--- main.c
+++ main.c
@@ -3,22 +3,29 @@
 #include "debug.h"
 #include "graphics.h"
 #include "types.h"
+#include "execute.h"
 
 int main(int argc,char **argv){
 	printf("Welcome to our 8086 Emulator...\n");
-	init(500,500);
-	SET_LOW(REG(AX),255);
-	SET_HI(REG(AX),12);
+	
 	//exec("DOSDEF.COM");
 	//exec("hello.com");
 	//exec("test.out");
 	//exec("add-sub.com");
 	//exec("calc-sum.com");
-	exec("hex-bin.com");
+	
 	//exec("hi-world.com");
 	//exec("pali.com");
     //exec("viksiege.exe");
     //exec("snake.com");
-	//run();
+
+
+
+    init(500,500);
+	SET_LOW(REG(AX),255);
+	SET_HI(REG(AX),12);
+	run();
+    //exec_com("hex-bin.com");
+	
 	//print_regs();
 }
